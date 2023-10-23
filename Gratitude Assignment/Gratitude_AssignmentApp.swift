ac4d15dd0a6a3f705ce846f101d19197fa9d588e
PIYUSH SINGH <piyushsingh9862@gmail.com>
2023-10-24 01:18:09 +0530

    Initial Commit

---
+++ Gratitude Assignment/Gratitude_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gratitude_AssignmentApp.swift
+//  Gratitude Assignment
+//
+//  Created by Piyush Singh on 24/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Gratitude_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
